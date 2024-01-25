9fc0414adefab207a01eb116e5f12f06d93d7fcd
ShohruhEgamov <shohruhwebdev@gmail.com>
2024-01-25 13:03:49 +0300

    Python ozgaruvchilar

--- w3chool.py
+++ w3chool.py
@@ -10,7 +10,24 @@ more than just one line
 """
 print("Hello, World!")
 
+
+#Python O`zgaruvchilar
 x = 5
 y = "John"
 print(x)
 print(y)
+
+#Agar siz o'zgaruvchining ma'lumotlar turini belgilamoqchi
+#bo'lsangiz, bu casting yordamida amalga oshirilishi mumkin.
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+#Funktsiya yordamida o'zgaruvchining ma'lumotlar turini olishingiz mumkin type()
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
